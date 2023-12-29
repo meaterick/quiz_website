55738d743e35daf2baa0743363688a7b96ad87ea
meaterick <89893748+meaterick@users.noreply.github.com>
2023-12-29 11:52:33 +0900

    Add files via upload

---
+++ lreaning_website/mainpage.js
@@ -0,0 +1,6 @@
+const buttons = calculator.querySelector('.home_button');
+
+buttons.addEventListener('click', function (event) {
+    const target = event.target;
+    const action = target.classList[0]
+}
