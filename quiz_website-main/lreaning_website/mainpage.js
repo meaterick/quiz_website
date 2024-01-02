5b628cc9eec220a348a8388f11447a791bada862
meaterick <89893748+meaterick@users.noreply.github.com>
2024-01-03 02:11:47 +0900

    Delete quiz_website-main directory

--- quiz_website-main/lreaning_website/mainpage.js
+++
@@ -1,6 +0,0 @@
-const buttons = calculator.querySelector('.home_button');
-
-buttons.addEventListener('click', function (event) {
-    const target = event.target;
-    const action = target.classList[0]
-}
